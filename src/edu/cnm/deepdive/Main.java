f715a41d89428570697c8fc1486b2d5fbe605e51
finn <isaaclindland@protonmail.com>
2019-09-25 13:10:43 -0600

    Added fibonacci big int

--- src/edu/cnm/deepdive/Main.java
+++ src/edu/cnm/deepdive/Main.java
@@ -13,5 +13,14 @@ public class Main {
     System.out.println(Palindromes.isPalindrome("race car"));
     System.out.println(Palindromes.isPalindrome("able was i ere i saw elba"));
     System.out.println(Palindromes.isPalindrome("Madam, I'm Adam"));
+
+    System.out.println(Fibonacci.fibonacci(10));
+    System.out.println(Fibonacci.fibonacci(5));
+    System.out.println(Fibonacci.fibonacci(2));
+    System.out.println(Fibonacci.fibonacci(6));
+    System.out.println(Fibonacci.fibonacci(7));
+
+
+
   }
 }
