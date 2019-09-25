64e11f6ea7342bb49bfee39f4caa05b7787a4bda
finn <isaaclindland@protonmail.com>
2019-09-25 14:09:14 -0600

    Added fibonacci big int

--- src/edu/cnm/deepdive/Fibonacci.java
+++ src/edu/cnm/deepdive/Fibonacci.java
@@ -1,15 +1,104 @@
 package edu.cnm.deepdive;
 
 import java.math.BigInteger;
+import java.util.Iterator;
+import java.util.Map;
 
-public class Fibonacci {
+public class Fibonacci implements Iterable<BigInteger>{
+
+  final int length;
+
+  public Fibonacci(int length) {
+    this.length = length;
+  }
+
+  public static BigInteger fibonacciOneLine(int n){
+    return n <= 1 ? BigInteger.valueOf(n) : fibonacciOneLine(n-1).add(fibonacciOneLine(n-2));
+  }
 
   public static BigInteger fibonacci(int n){
+    BigInteger f;
 
-    return n <= 1 ? BigInteger.valueOf(n) : fibonacci(n-1).add(fibonacci(n-2));
+    if(n==0) {
+      f = BigInteger.ZERO;
+    } else if(n==1){
+      f = BigInteger.ONE;
+    } else {
+      f = fibonacci(n-1).add(fibonacci(n-2));
+    }
+    return f;
+  }
 
+  public static BigInteger fibonacciMemoized(int n, Map<Integer, BigInteger> memo){
+
+    BigInteger f;
+
+    if(memo.containsKey(n)){
+      return memo.get(n);
+    }
+
+    if(n==0) {
+      f = BigInteger.ZERO;
+    } else if(n==1){
+      f = BigInteger.ONE;
+    } else {
+      f = fibonacciMemoized(n-1,memo).add(fibonacciMemoized(n-2,memo));
+    }
+    memo.put(n,f);
+    return f;
+  }
+
+  public static BigInteger fibonacciIterative(int n){
+    BigInteger prev = BigInteger.ZERO;
+    BigInteger curr = BigInteger.ONE;
+
+    if(n <= 0){
+      return prev;
+    }
+    for (int i = 2; i < n; i++) {
+      BigInteger next = prev.add(curr);
+      prev = curr;
+      curr = next;
+
+
+    }
+    return curr;
 
   }
 
 
+
+
+
+  @Override
+  public Iterator<BigInteger> iterator() {
+    return new FibIterator(length);
+  }
+  private class FibIterator implements Iterator<BigInteger>{
+
+    private final int length;
+    private int counter;
+    private BigInteger prev = BigInteger.valueOf(-1);
+    private BigInteger curr = BigInteger.ONE;
+
+
+    private FibIterator(int length) {
+      this.length = length;
+      counter = length;
+    }
+
+    @Override
+    public boolean hasNext() {
+      return counter > 0;
+    }
+
+    @Override
+    public BigInteger next() {
+      BigInteger next = prev.add(curr);
+      prev = curr;
+      curr = next;
+      counter--;
+      return curr;
+    }
+  }
 }
--- src/edu/cnm/deepdive/Main.java
+++ src/edu/cnm/deepdive/Main.java
@@ -1,9 +1,12 @@
 package edu.cnm.deepdive;
 
+import java.math.BigInteger;
+import java.util.HashMap;
+
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println(Factorial.fact(4000));
+    System.out.println(Factorial.fact(20));
 
     System.out.println(Recursion.countPairs("axaxbxax"));
     System.out.println(Recursion.countPairs("axxbxax"));
@@ -14,11 +17,30 @@ public class Main {
     System.out.println(Palindromes.isPalindrome("able was i ere i saw elba"));
     System.out.println(Palindromes.isPalindrome("Madam, I'm Adam"));
 
+    System.out.println(Fibonacci.fibonacciOneLine(10));
+    System.out.println(Fibonacci.fibonacciOneLine(5));
+    System.out.println(Fibonacci.fibonacciOneLine(2));
+    System.out.println(Fibonacci.fibonacciOneLine(6));
+    System.out.println(Fibonacci.fibonacciOneLine(7));
+    System.out.println(Fibonacci.fibonacciOneLine(-10));
+    System.out.println();
     System.out.println(Fibonacci.fibonacci(10));
     System.out.println(Fibonacci.fibonacci(5));
     System.out.println(Fibonacci.fibonacci(2));
     System.out.println(Fibonacci.fibonacci(6));
     System.out.println(Fibonacci.fibonacci(7));
+
+    System.out.println();
+    System.out.println(Fibonacci.fibonacciMemoized(1499, new HashMap<>()));
+
+    System.out.println();
+    System.out.println(Fibonacci.fibonacciIterative(1500));
+
+
+    Fibonacci fibonacci = new Fibonacci(100);
+    for (BigInteger bigInteger : fibonacci) {
+      System.out.println(bigInteger);
+    }
 
 
 
